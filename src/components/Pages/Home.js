f9b41aee030f58bad82ccdac53a211d99ed8a784
SamsonI95 <samsoniweibo@gmail.com>
2023-12-25 10:21:20 +0100

    This commit contains the webpage being developed using React

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../App";
+import HeroSection from "../HeroSection";
+
+
+function Home() {
+  return <HeroSection></HeroSection>;
+}
+
+export default Home;
---
+++ src/components/SidebarData.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./SidebarData.css";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "Profile Information",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "Doctor's chat",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "Pharmacy",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "Test Labs",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "Health Insurance",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "About us",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "Privacy Policy",
+    path: "/",
+    cName: "nav-text",
+  },
+];
